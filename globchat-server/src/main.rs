6577d7eb4c0e4a2c1236c4ae8d1bfcb182c6be6b
Maxuss <maxus.info.mail@gmail.com>
2023-08-20 12:29:42 +0300

    basic server setup

--- globchat-server/src/main.rs
+++ globchat-server/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use tracing::info;
+use tracing_subscriber::prelude::*;
+
+fn prepare_logging() -> anyhow::Result<()> {
+    let stdout_log = tracing_subscriber::fmt::layer().compact();
+
+    tracing_subscriber::registry()
+        .with(
+            stdout_log
+                .with_filter(tracing_subscriber::filter::LevelFilter::DEBUG)
+                .with_filter(tracing_subscriber::filter::filter_fn(|metadata| {
+                    metadata
+                        .module_path()
+                        .unwrap_or("unknown")
+                        .starts_with("globchat_server")
+                })),
+        )
+        .init();
+
+    Ok(())
+}
+
+#[tokio::main]
+#[tracing::instrument]
+async fn main() -> anyhow::Result<()> {
+    prepare_logging()?;
+
+    info!("Hello, world!");
+
+    Ok(())
 }
